01bbcddeeb8df5a7dced29bb7a71be84dd3c2f6e
TechnologicNick <nickvanluijk@gmail.com>
2019-01-06 00:40:10 +0100

    Finished OptionsMenuHandler (I hope)

--- Scripts/OptionsMenuHandler.lua
+++ Scripts/OptionsMenuHandler.lua
@@ -1,18 +1,118 @@
-print("OptionsMenuHandler Reloaded!")
+--print("OptionsMenuHandler Reloaded!")
 
-function OptionsMenuItem(scriptedClass, widgetAll, id, name, displayName, options, defaultIndex)
+function OptionsMenu(scriptedClass, title, rowsPerColumn)
+    local object = {}
+    
+    object.scriptedClass = scriptedClass
+    object.title = title
+    object.rowsPerColumn = rowsPerColumn or 9
+    object.items = {}
+    object.decreaseButtons = {}
+    object.increaseButtons = {}
+    
+    function object.setupGui(self)
+        self.guiBg = sm.gui.load("ChallengeMessage.layout", true)
+        self.gui = sm.gui.load("OptionsMenuPage.layout", false)
+        
+        self.guiBg.bgMainPanel = self.guiBg:find("MainPanel")
+        self.gui.mainPanel = self.gui:find("OptionsMenuPageMainPanel")
+        
+        self.guiBg.bgMainPanel.title = self.guiBg.bgMainPanel:find("Title")
+        self.guiBg.bgMainPanel.btnNext = self.guiBg.bgMainPanel:find("Next")
+        self.guiBg.bgMainPanel.btnReset = self.guiBg.bgMainPanel:find("Reset")
+        
+        self:resize(nil)
+        
+        -- Hide all items
+        for i=0,17 do
+            self.gui.mainPanel:find("ITEM_" .. i).visible = false
+        end
+    end
+    
+    function object.resize(self, wideMode)
+        if wideMode == nil then
+            wideMode = #object.items > self.rowsPerColumn
+        end
+    
+        local screenWidth, screenHeight = sm.gui.getScreenSize()
+        local bgWidth = wideMode and 1440 or 739
+        local bgHeight = 163 + 53 * self:getRowCount()
+        local bgPosX = screenWidth/2 - bgWidth/2
+        local bgPosY = screenHeight/2 - bgHeight/2
+        
+        self.guiBg.bgMainPanel:setSize(bgWidth, bgHeight)
+        self.guiBg.bgMainPanel:setPosition(bgPosX, bgPosY)
+        
+        self.guiBg.bgMainPanel.title:setPosition(0, 0)
+        self.guiBg.bgMainPanel.title:setSize(bgWidth, 90)
+        self.guiBg.bgMainPanel.title:setText(self.title)
+        
+        self.guiBg.bgMainPanel.btnNext.visible = false
+        
+        self.guiBg.bgMainPanel.btnReset.visible = false
+        
+        self.gui.mainPanel:setSize(bgWidth, bgHeight)
+        self.gui.mainPanel:setPosition(bgPosX, bgPosY)
+    end
+    
+    function object.getRowCount(self)
+        local highestRow = 0
+        for k,v in pairs(self.items) do
+            local currentRow = v.rowIndex % self.rowsPerColumn
+            if currentRow > highestRow then
+                highestRow = currentRow
+            end
+        end
+        return highestRow
+    end
+    
+    function object.show(self)
+        self.guiBg.visible = true
+    end
+    
+    function object.hide(self)
+        self.guiBg.visible = false
+    end
+    
+    function object.update(self, dt)
+        if self.gui and self.guiBg then
+            self.gui.visible = self.guiBg.visible
+        end
+    end
+    
+    function object.addOptionsMenuItem(self, id, rowIndex, name, displayName, options, defaultIndex)
+        self.items[id] = OptionsMenuItem(self, id, rowIndex, name, displayName, options, defaultIndex)
+    end
+    
+    return object
+end
+
+
+
+--[[
+    optionsMenu  = OptionsMenu - The OptionsMenu. (parent)
+    id           = Number      - The id of the item.
+    rowIndex     = Number      - The index to draw the item in the GUI. (use 'nil' to default 'rowIndex' to 'id')
+    name         = String      - The name used in storage. (not implemented yet)
+    displayName  = String      - The name to be displayed in the GUI. (use 'nil' to default 'displayName' to 'name')
+    options      = {String}    - Table of strings of options to choose from. Has support for i18n. (use 'nil' to default to "On/Off")
+    defaultIndex = Number      - The index of the default option. (use 'nil' to default to the first option)
+]]
+function OptionsMenuItem(optionsMenu, id, rowIndex, name, displayName, options, defaultIndex)
     -- Create object
     local object = {}
     
     -- Initialisation
-    object.scriptedClass = scriptedClass
-    object.widgetAll = widgetAll
+    object.optionsMenu = optionsMenu
+    object.widgetAll = object.optionsMenu.gui.mainPanel
     object.id = id
+    object.rowIndex = rowIndex or object.id
     object.name = name
     object.displayName = displayName or name
     object.options = options or {"#{MENU_OPTION_ON}", "#{MENU_OPTION_OFF}"}
     object.defaultIndex = defaultIndex or 1
     
+    object.scriptedClass = optionsMenu.scriptedClass
     object.widgetItem = object.widgetAll:find("ITEM_" .. tostring(id))
     object.selectedIndex = object.defaultIndex
     
@@ -21,55 +121,66 @@ function OptionsMenuItem(scriptedClass, widgetAll, id, name, displayName, option
     object.btnDecrease = object.widgetItem:find("Decrease")
     object.btnIncrease = object.widgetItem:find("Increase")
     
-    object.scriptedClass.decreaseButtons[object.btnDecrease.id] = object.id
-    object.scriptedClass.increaseButtons[object.btnIncrease.id] = object.id
+    object.optionsMenu.decreaseButtons[object.btnDecrease.id] = object.id
+    object.optionsMenu.increaseButtons[object.btnIncrease.id] = object.id
     
     -- OptionsMenuItem Functions
-    function object.selectIndex(index)
-        --print(object.selectedIndex, index)
-        object.selectedIndex = index
-        object.tbValue:setText(object.options[object.selectedIndex])
-    end
-    
-    function object.decrease()
-        --object.selectIndex(((object.selectedIndex + 2) % (#object.options + 0)) + 1)
-        local tmp = object.selectedIndex - 1
-        if tmp < 1 then
-            tmp = #object.options
+    function object.selectIndex(self, index)
+        --print(type(index))
+        --print("1", index)
+        self.selectedIndex = index
+        if not sm.isServerMode() then
+            --print("2", sm.isServerMode(), self.selectedIndex)
+            self.tbValue:setText(tostring(self.options[self.selectedIndex]))
         end
-        object.selectIndex(tmp)
-        --print("Decrease the value!", (object.selectedIndex + 1) % #object.options))
     end
     
-    function object.increase()
-        object.selectIndex(object.selectedIndex % #object.options + 1)
-        --print("Increase the value!", (object.selectedIndex + 1) % #object.options))
+    function object.decrease(self)
+        local newIndex = self.selectedIndex - 1
+        if newIndex < 1 then
+            newIndex = #self.options
+        end
+        self.selectIndex(object, newIndex)
+        self.optionsMenu.scriptedClass.network:sendToServer("server_onOptionsMenuValueChanged", {id = self.id, selectedIndex = newIndex, change = -1})
+    end
+    
+    function object.increase(self)
+        local newIndex = self.selectedIndex % #self.options + 1
+        self.selectIndex(self, newIndex)
+        self.optionsMenu.scriptedClass.network:sendToServer("server_onOptionsMenuValueChanged", {id = self.id, selectedIndex = newIndex, change = 1})
+    end
+    
+    function object.getSelectedIndex()
+        return object.selectedIndex
+    end
+    
+    function object.getSelectedOption()
+        return object.options[object.selectedIndex]
     end
     
     object.widgetItem.visible = true
     object.widgetItem.posY = select(2, object.widgetItem:getPosition())
-    --print("here", object.widgetItem.posY, object.widgetItem:getPosition())
     
     -- Widget parameters
     object.widgetItem.height = 50
-    object.widgetItem.horizontalDivider = 3
+    object.widgetItem.rowDivider = 3
     object.widgetItem.buttonWidth = 27
     object.widgetItem.buttonHeight = 40
     
     
     
     object.widgetItem:setSize(720, object.widgetItem.height)
-    if object.id < 9 then
-        object.widgetItem:setPosition(40, 92 + (object.widgetItem.height + object.widgetItem.horizontalDivider) * object.id)
+    if object.id < object.optionsMenu.rowsPerColumn then
+        object.widgetItem:setPosition(40, 92 + (object.widgetItem.height + object.widgetItem.rowDivider) * object.rowIndex)
     else
-        object.widgetItem:setPosition(741, 92 + (object.widgetItem.height + object.widgetItem.horizontalDivider) * (object.id - 9))
+        object.widgetItem:setPosition(741, 92 + (object.widgetItem.height + object.widgetItem.rowDivider) * (object.rowIndex - object.optionsMenu.rowsPerColumn))
     end
     
     object.tbLabel:setPosition(0, 0)
     object.tbLabel:setSize(720, object.widgetItem.height)
     
     object.tbLabel:setText(object.displayName)
-    object.selectIndex(object.defaultIndex)
+    object:selectIndex(object.defaultIndex)
     
     object.btnDecrease:setSize(object.widgetItem.buttonWidth, object.widgetItem.buttonHeight)
     object.btnIncrease:setSize(object.widgetItem.buttonWidth, object.widgetItem.buttonHeight)
@@ -86,12 +197,12 @@ function OptionsMenuItem(scriptedClass, widgetAll, id, name, displayName, option
     --print("decrease =", scriptedClass.decrease)
     
     -- ScriptedClass Functions
-    function scriptedClass.decrease(self, widget)
-        self.optionsMenuItems[self.decreaseButtons[widget.id]]:decrease()
+    function object.scriptedClass.decrease(self, widget)
+        self.optionsMenu.items[self.optionsMenu.decreaseButtons[widget.id]]:decrease()
     end
     
-    function scriptedClass.increase(self, widget)
-        self.optionsMenuItems[self.increaseButtons[widget.id]]:increase()
+    function object.scriptedClass.increase(self, widget)
+        self.optionsMenu.items[self.optionsMenu.increaseButtons[widget.id]]:increase()
     end
     
     return object
--- Scripts/ScrapGuard.lua
+++ Scripts/ScrapGuard.lua
@@ -2,101 +2,94 @@ dofile("OptionsMenuHandler.lua")
 
 -- ScrapGuard.lua --
 
---print("[ScrapGuard] file init")
-
 ScrapGuard = class( nil )
-ScrapGuard.maxChildCount = -1
+ScrapGuard.maxChildCount = 0
 ScrapGuard.maxParentCount = -1
-ScrapGuard.connectionInput = sm.interactable.connectionType.logic + sm.interactable.connectionType.power
-ScrapGuard.connectionOutput = sm.interactable.connectionType.logic
+ScrapGuard.connectionInput = sm.interactable.connectionType.seated
+ScrapGuard.connectionOutput = sm.interactable.connectionType.none
 ScrapGuard.colorNormal = sm.color.new( 0x404040ff )
 ScrapGuard.colorHighlight = sm.color.new( 0x606060ff )
 
 function ScrapGuard.client_onCreate( self )
-    self.optionsMenuItems = {}
-    self.decreaseButtons = {}
-    self.increaseButtons = {}
+    self.optionsMenu = OptionsMenu(self, "Scrap Guard", 9)
 end
 
 function ScrapGuard.client_onSetupGui( self )
-    --self.guiAntiHUD = sm.gui.load("ChallengeMessage.layout", true)
-    self.guiBg = sm.gui.load("ChallengeMessage.layout", true)
-    self.gui = sm.gui.load("OptionsMenuPage.layout", false)
+    self.optionsMenu:setupGui()
     
-    self.guiBg.bgMainPanel = self.guiBg:find("MainPanel")
-    self.gui.mainPanel = self.gui:find("OptionsMenuPageMainPanel")
+    self.optionsMenu:addOptionsMenuItem(0,  0, "Out of world protection", nil, nil, nil)
+    self.optionsMenu:addOptionsMenuItem(1,  2, "Connectable", nil, nil, nil)
+    self.optionsMenu:addOptionsMenuItem(2,  3, "Desctructable", nil, nil, nil)
+    self.optionsMenu:addOptionsMenuItem(3,  4, "Buildable", nil, nil, nil)
+    self.optionsMenu:addOptionsMenuItem(4,  5, "Paintable", nil, nil, nil)
+    self.optionsMenu:addOptionsMenuItem(5,  6, "Liftable", nil, nil, nil)
+    --self.optionsMenu:addOptionsMenuItem(6,  nil, "Boolean test", nil, nil, nil)
+    --self.optionsMenu:addOptionsMenuItem(7,  nil, "Boolean test", nil, nil, nil)
+    --self.optionsMenu:addOptionsMenuItem(8,  nil, "Boolean test", nil, nil, nil)
+    --self.optionsMenu:addOptionsMenuItem(9,  nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
+    --self.optionsMenu:addOptionsMenuItem(10, nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
+    --self.optionsMenu:addOptionsMenuItem(11, nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
+    --self.optionsMenu:addOptionsMenuItem(12, nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
+    --self.optionsMenu:addOptionsMenuItem(13, nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
+    --self.optionsMenu:addOptionsMenuItem(14, nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
+    --self.optionsMenu:addOptionsMenuItem(15, nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
+    --self.optionsMenu:addOptionsMenuItem(16, nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
+    --self.optionsMenu:addOptionsMenuItem(17, nil, "Number test", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
     
-    local screenWidth, screenHeight = sm.gui.getScreenSize()
-    --local bgWidth, bgHeight = self.guiBg.bgMainPanel:getSize()
-    local bgWidth = 1440
-    local bgHeight = 640
-    self.guiBg.bgMainPanel:setSize(bgWidth, bgHeight)
-    
-    --local bgPosX, bgPosY = self.guiBg.bgMainPanel:getPosition()
-    local bgPosX = screenWidth/2 - bgWidth/2
-    local bgPosY = screenHeight/2 - bgHeight/2
-    
-    --print(bgPosX, bgPosY, screenWidth, screenHeight, bgWidth, bgHeight)
-    
-    --self.guiBg.bgMainPanel:setSize(bgWidth, bgHeight)
-    self.guiBg.bgMainPanel:setPosition(bgPosX, bgPosY)
-    self.guiBg.bgMainPanel.title = self.guiBg.bgMainPanel:find("Title")
-    self.guiBg.bgMainPanel.title:setText("Scrap Guard")
-    self.guiBg.bgMainPanel.btnNext = self.guiBg.bgMainPanel:find("Next")
-    self.guiBg.bgMainPanel.btnNext.visible = false
-    self.guiBg.bgMainPanel.btnReset = self.guiBg.bgMainPanel:find("Reset")
-    self.guiBg.bgMainPanel.btnReset.visible = false
-    
-    self.gui.mainPanel:setSize(bgWidth, bgHeight)
-    self.gui.mainPanel:setPosition(bgPosX, bgPosY)
-    
-    
-    
-    for i=0,17 do
-        self.gui.mainPanel:find("ITEM_" .. i).visible = false
-    end
-    
-    self.optionsMenuItems[0]  = OptionsMenuItem(self, self.gui.mainPanel, 0,  "Shadow", nil, {"#{MENU_OPTION_LOW}", "#{MENU_OPTION_MEDIUM}", "#{MENU_OPTION_HIGH}"}, nil)
-    self.optionsMenuItems[1]  = OptionsMenuItem(self, self.gui.mainPanel, 1,  "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[2]  = OptionsMenuItem(self, self.gui.mainPanel, 2,  "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[3]  = OptionsMenuItem(self, self.gui.mainPanel, 3,  "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[4]  = OptionsMenuItem(self, self.gui.mainPanel, 4,  "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[5]  = OptionsMenuItem(self, self.gui.mainPanel, 5,  "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[6]  = OptionsMenuItem(self, self.gui.mainPanel, 6,  "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[7]  = OptionsMenuItem(self, self.gui.mainPanel, 7,  "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[8]  = OptionsMenuItem(self, self.gui.mainPanel, 8,  "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[9]  = OptionsMenuItem(self, self.gui.mainPanel, 9,  "Dynamic Lights", nil, nil, nil)
-    self.optionsMenuItems[10] = OptionsMenuItem(self, self.gui.mainPanel, 10, "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[11] = OptionsMenuItem(self, self.gui.mainPanel, 11, "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[12] = OptionsMenuItem(self, self.gui.mainPanel, 12, "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[13] = OptionsMenuItem(self, self.gui.mainPanel, 13, "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[14] = OptionsMenuItem(self, self.gui.mainPanel, 14, "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[15] = OptionsMenuItem(self, self.gui.mainPanel, 15, "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[16] = OptionsMenuItem(self, self.gui.mainPanel, 16, "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    self.optionsMenuItems[17] = OptionsMenuItem(self, self.gui.mainPanel, 17, "Shadow2", nil, {"1", "2", "3", "4", "5", "6", "7"}, nil)
-    
-    print(sm.gui.load("GraphicsOptions.layout", false):find("GraphicsMainPanel"):find("GraphicsQuality"):getSize())
-    
-    --for i=0,1 do
-    --    print(self.optionsMenuItems[i])
-    --end
+    self.optionsMenu:resize(nil)
 end
 
 function ScrapGuard.client_onInteract( self )
-    if self.gui and self.guiBg then
-        self.guiBg.visible = true
-        --self.gui.visible = true
-        
-    end
+    self.optionsMenu:show()
 end
 
 function ScrapGuard.client_onUpdate( self, dt )
-    if self.gui and self.guiBg then
-        self.gui.visible = self.guiBg.visible
-        --self.guiBg.visible = self.gui.visible
+    self.optionsMenu:update(dt)
+end
+
+function ScrapGuard.client_onOptionsMenuValueChanged( self, data )
+    if sm.isHost then
+        self.optionsMenu.items[data.id]:selectIndex(data.selectedIndex)
     end
 end
 
---function ScrapGuard.decrease( self, widget )
---    print("decrease", self, idk, idk2)
---end
+function ScrapGuard.server_onOptionsMenuValueChanged( self, data )
+    --print(data, data.selectedIndex, type(data.selectedIndex))
+    self.optionsMenu.items[data.id]:selectIndex(data.selectedIndex)
+    self.network:sendToClients("client_onOptionsMenuValueChanged", data)
+end
+
+
+
+function ScrapGuard.server_onFixedUpdate( self, timeStep )
+    if self.optionsMenu.items[0] ~= nil then
+        if self.optionsMenu.items[0]:getSelectedIndex() == 1 then
+            local position = self.shape:getWorldPosition()
+            if position:length2() > 2000000 then
+                print("Put it on a lift")
+                sm.player.placeLift(
+                    server_getNearestPlayer(position),
+                    self.shape.body:getCreationBodies(),
+                    sm.vec3.new(0, 0, 0),
+                    0,
+                    0
+                )
+            end
+        end
+    end
+end
+
+function server_getNearestPlayer( position )
+    local nearestPlayer = nil
+    local nearestDistance = nil
+    for id,player in pairs(sm.player.getAllPlayers()) do
+        if sm.exists(player.character) then
+            local length2 = sm.vec3.length2(position - player.character:getWorldPosition())
+            if nearestDistance == nil or length2 < nearestDistance then
+                nearestDistance = length2
+                nearestPlayer = player
+            end
+        end
+    end
+    return nearestPlayer
+end
